dc5f21e706af3250f405b20d00c3d2a4e9328233
srinivasan0077 <srinivasandhandapani071201@gmail.com>
2025-11-16 21:27:48 +0530

    Added authentication,form validation and improved accessibility

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,7 +1,7 @@
 import DashboardSkeleton from "../../ui/skeletons";
 
 
-
+//Used as a fallback when original component being built by next js on server 
 export default function Loading(){
 
     return <DashboardSkeleton/>;
